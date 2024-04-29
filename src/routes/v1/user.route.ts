a8853334d75b89836eb3f2a08cde11548de18c6b
Ken Gervaico <johnkgervacio@gmail.com>
2024-04-29 15:33:55 +0800

    revert

--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -7,12 +7,12 @@ import { userController } from '../../controllers';
 const router = express.Router();
 
 router
-  .route('/users/')
+  .route('/')
   .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
   .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
 
 router
-  .route('/users/:userId')
+  .route('/:userId')
   .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
   .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
   .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
